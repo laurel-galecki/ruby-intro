a954c5101052ae47312514af3854104bc84caad0
Laurel Galecki <laurel.galecki@kellogg.northwestern.edu>
2023-04-20 03:36:46 +0000

    end of class

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,10 +2,27 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
-# Infinite Tacos -> Finite Tacos
+# Infinite Tacos -> Finite Tacos ctrl C gets you out
 # loop do
 #   puts "tacos!"
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+index = 0
+loop do
+    if index == tacos.count
+        break
+    end
+    taco = tacos[index]
+    puts "#{taco} tacos!"
+index = index + 1
+end
+
+# for loop - represents the element we reach into in array, shorter code
+# for ___ in ___ end
+
+for taco in tacos
+    puts "#{taco} tacos!"
+end
